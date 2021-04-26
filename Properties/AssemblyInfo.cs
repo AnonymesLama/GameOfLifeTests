2e2eb57bb4daf7e5267b0bb55a878342cd4168fa
AnonymesLama <merianduhla@gmail.com>
2021-04-26 09:30:14 +0200

    test-projekt

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameOfLifeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameOfLifeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e0c07ac-335e-4b9e-b4c9-eecf2ea3fd21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WPF_GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSetStatus_2x2_ToteZellen()
+        {
+            //Arrange
+            GameOfLife gol = new GameOfLife();
+            int breite = 2;
+            int hoehe = 2;
+            Feld[,] feld = new Feld[hoehe, breite];
+            for (int i = 0; i < hoehe; i++)
+            {
+                for (int j = 0; j < breite; j++)
+                {
+                    feld[i, j] = new Feld(new Rectangle(), false);
+                    String a = feld[i, j].ToString();
+                }
+            }
+            int[,] soll = { { }, { }, }; //gewünschstes Ergebnis der Methode
+
+            //Act
+            int[,] test = gol.Calculate_Cells(feld, hoehe, breite);
+
+            //Assert
+            Assert.AreEqual(soll, test);
+        }
+    }
+}
