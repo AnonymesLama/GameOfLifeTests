5b51761bc6d7c06f09c1baacb54fc63fcf84e44b
AnonymesLama <merianduhla@gmail.com>
2021-05-17 14:08:38 +0200

    Tests

---
+++ GameOfLifeCheckTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WPF_GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GameOfLifeCheckTest
+    {
+        [TestMethod]
+        public void setAbove1()
+        {
+            //Arrange
+            GameOfLife gol = new GameOfLife(3,3);
+
+            //Act
+            gol.setAbove(1);
+
+            //Assert
+            Assert.AreEqual(gol.getAbove(), 1);
+        }
+
+        [TestMethod]
+        public void setBelow12()
+        {
+            //Arrange
+            GameOfLife gol = new GameOfLife(14, 14);
+
+            //Act
+            gol.setBelow(12);
+
+            //Assert
+            Assert.AreEqual(gol.getBelow(), 12);
+        }
+
+        [TestMethod]
+        public void setLeft45()
+        {
+            //Arrange
+            GameOfLife gol = new GameOfLife(50, 50);
+
+            //Act
+            gol.setLeft(45);
+
+            //Assert
+            Assert.AreEqual(gol.getLeft(), 45);
+        }
+    }
+}
